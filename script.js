53dce59b70ff0708b8cb98f80e0a7cd6bb92f277
abangdsm <id.reversal@gmail.com>
2023-01-15 19:49:02 +0700

    Menambahkan Constructor

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Function Literal Pada JavaScript
+
 /*let data = {
     nama : "Dwi Star Muda",
     member : "Gold",
@@ -9,7 +11,9 @@
     }
 }*/
 
-function member (nama, poin, kota) {
+// Function Declaration pada JavaScript
+
+/*function member (nama, poin, kota) {
     const data = {};
     data.nama = nama;
     data.poin = poin;
@@ -20,4 +24,19 @@ function member (nama, poin, kota) {
 const member1 = member('Dwi Star Muda', 200, 'Tanjung Morawa');
 console.log(member1);
 console.log(member1.kota);
-console.log(member1['poin']);
+console.log(member1['poin']);*/
+
+// Function Constructor pada JavaScript
+
+/*function member (nama, poin, kota) {
+    this.nama = nama;
+    this.poin = poin;
+    this.kota = kota;
+}
+
+let data = new member('Dwi Star Muda', 1500, 'Medan');
+console.log(data);
+console.log(data.kota);
+console.log(data.poin);
+console.log(data.nama);*/
+
